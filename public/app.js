bbecf70bee006bd8c831ea1995e21f3e85fbdc0b
Isaac BA <iba.burton@gmail.com>
2017-01-10 11:01:08 +0200

    yo

---
+++ public/app.js
@@ -0,0 +1,22 @@
+var app  = angular.module('repoApp',['ui.router']);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('login',{
+        url:'/',
+        controller:'LoginCtrl',
+        templateUrl:'/templates/login.html'
+      })
+      .state('userData',{
+        
+        
+      })
+ }
+
+
+  ])
