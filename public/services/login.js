416a901e7bb130fc87cccff9253fc8eacd260dc4
Isaac BA <iba.burton@gmail.com>
2017-01-10 17:56:54 +0200

    login

--- public/services/login.js
+++ public/services/login.js
@@ -3,11 +3,15 @@ app.factory('login',['$http', function($http){
   loginService = {
     user:{},
     click:function(user){
-      $http.post('/login',user).then(function(data){
-      // console.log(data.data);
-      angular.copy(data.data , loginService.user);
-      // console.log(loginService.user)
-    })
+    //   $http.post('/login',user).then(function(data){
+    //   // console.log(data.data);
+    //   angular.copy(data.data , loginService.user);
+    //   // console.log(loginService.user)
+    // })
+    //   console.log(user);
+    // $http.get('/auth/github').then(function(data){
+    //   console.log(data);
+    // })
     }
   }
   
