ced29ff34ba9bf0cd4f1d9f9461b3e9bc647c114
Maor <maorfeldinger@gmail.com>
2017-01-11 19:13:25 +0200

    delete comment

--- server.js
+++ server.js
@@ -28,34 +28,7 @@ var POSURL = "?client_id=" + CLIENTID +"&client_secret=" + CLIENTSECRET;
 
 /**************middleware***********************/
 
-// router.get('/issues', function(req, res) {
-//         var data = [];
-//         var getData = function(pageCounter) {
-//             request(options, function(error, response, body) {
-//                 if(!error && response.statusCode === 200) {
-//                     // for(var issueIndex = 0; issueIndex < body.length; issueIndex++) {
-//                     //     if(!body[issueIndex].pull_request) {
-//                     //         issueData.push({
-//                     //             number: body[issueIndex].number,
-//                     //             title: body[issueIndex].title,
-//                     //             state: body[issueIndex].state,
-//                     //             creator: body[issueIndex].user.login,
-//                     //             assignee: body[issueIndex].assignee ? body[issueIndex].assignee.login : ''
-//                     //         });
-//                     //     }
-//                     // }
 
-//                     if(body.length < 30) {
-//                         res.send(issueData);
-//                     } else {
-//                         getData(pageCounter + 1);
-//                     }
-//                 }
-//             });
-//         };
-//         getData(1);
-//     });
-// };
 
 /*************API Functionality***************/
 
